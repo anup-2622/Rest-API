7b4e9d7081f32444f1c75c963490f88d8def58b8
Anup <anupisri107@gmail.com>
2024-01-04 01:44:12 +0530

    UTF-8 problem solved

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 require("dotenv").config();
+
 const express = require("express");
+
 const app = express();
+
 const products_routes = require("./routes/products");
+
 const PORT = process.env.PORT || 5000;
-// const connectDB = require("./db/connect");
-const mongoose = require("mongoose");
+
+const connectDB = require("./db/connect");
+
+// const mongoose = require("mongoose");
 
 app.get("/", (req, res) => {
   res.send("Hello E-com API");
@@ -12,14 +18,10 @@ app.get("/", (req, res) => {
 
 app.use("/api/products", products_routes);
 
-const connectDB = (uri) => {
-  console.log("db connected");
-  mongoose.connect(uri);
-};
-
 const startServer = async () => {
   try {
     await connectDB(process.env.MONOGDB_URL);
+
     app.listen(PORT, () => {
       console.log(`${PORT} Server is running`);
     });
